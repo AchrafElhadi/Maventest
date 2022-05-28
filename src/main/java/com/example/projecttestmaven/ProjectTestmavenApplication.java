fc5d291e6fef89fa3f7bc22a99916d7a9d09fae3
achrafel <achraf.elhadi@gmail.com>
2022-05-28 14:55:14 +0100

    first

---
+++ src/main/java/com/example/projecttestmaven/ProjectTestmavenApplication.java
@@ -0,0 +1,13 @@
+package com.example.projecttestmaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectTestmavenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectTestmavenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/projecttestmaven/ProjectTestmavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projecttestmaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectTestmavenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
